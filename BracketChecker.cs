3a7626943a0fa16d1e837af9ab623f2714b3b184
Brian Russell <Brianrussell2017@yahoo.com>
2024-12-05 10:14:19 -0500

    Add project files.

---
+++ BracketChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeProblem4_2_2
+{
+    public class BracketChecker
+    {
+
+        public static bool IsValid()
+        {
+            string input = Console.ReadLine();
+            Stack<char> stk = new Stack<char>();
+            foreach (var c in input.ToCharArray())
+            {
+                if (c == '(')
+                {
+                    stk.Push(')');
+                }
+                else if (c == '[')
+                {
+                    stk.Push(']');
+                }
+                else if (c == '{')
+                {
+                    stk.Push('}');
+                }
+                else if (stk.Count == 0 || stk.Pop() != c)
+                {
+                    return false;
+                }
+            }
+            return stk.Count == 0;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ChallengeProblem4_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string");
+          
+            Console.WriteLine(BracketChecker.IsValid());
+        }
+    }
+}
